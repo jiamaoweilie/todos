1d4ee0838bdb176b4ff374358954bb3c301a8471
Jia Wei <wjia@thoughtworks.com>
2014-12-23 23:08:40 +0800

    clean code

--- target/classes/static/js/todo-service.js
+++
@@ -1,6 +0,0 @@
-app.factory('TodoService', ['$resource', function($resource){
-    return $resource('/todos/:id', {}, {
-        'query' : {method: 'GET', isArray: true},
-        'delete': {method: 'DELETE', params: {id: '@id'}}
-    });
-}]);
