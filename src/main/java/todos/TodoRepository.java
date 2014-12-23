c8d9f3258ca05216574c7920bd5e6bb10e5acaf3
Jia Wei <wjia@thoughtworks.com>
2014-12-23 23:16:50 +0800

    delete some unuse code

--- src/main/java/todos/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package todos;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface TodoRepository extends CrudRepository<Todo, Long> {
-}
